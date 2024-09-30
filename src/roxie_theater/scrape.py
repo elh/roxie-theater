33b0d68624233a547c460884f81d24f029f7591f
elh <elh.is.online@gmail.com>
2024-09-30 00:37:50 -0700

    take over. fix a selector bug and aggregate movies

--- src/roxie_theater/scrape.py
+++ src/roxie_theater/scrape.py
@@ -8,23 +8,37 @@ def main():
     response = requests.get(url)
     soup = BeautifulSoup(response.content, "html.parser")
 
-    movies = []
+    month_year = soup.select_one(".calendar-block__month-title").text.strip()
+    month = month_year.split()[0]
+    year = month_year.split()[1]
+
+    movies = {}
     for day in soup.select(".calendar-day-item"):
         day_number = day.select_one(".calendar-day").text.strip()
         for film in day.select(".film"):
+            link = film.select_one("a")["href"]
             title = film.select_one(".film-title").text.strip()
-            link = film.select_one(".film-title a")["href"]
-            showtime = film.select_one(".film-showtime").text.strip()
-            movies.append(
-                {
-                    "name": title,
-                    "link": link,
-                    "showtimes": [f"September {day_number} {showtime}"],
-                }
-            )
 
-    output = {"movies": movies}
-    print(json.dumps(output, indent=2))
+            showtime = film.select_one(".film-showtime").text.strip()  # e.g. "6:20 pm"
+
+            showtime_obj = {
+                "year": year,
+                "month": month,
+                "day": day_number,
+                "showtime": showtime,
+            }
+
+            if link in movies:
+                movies[link]["showtimes"].append(showtime_obj)
+                continue
+
+            movies[link] = {
+                "name": title,
+                "link": link,
+                "showtimes": [showtime_obj],
+            }
+
+    print(json.dumps(movies, indent=2))
 
 
 if __name__ == "__main__":
